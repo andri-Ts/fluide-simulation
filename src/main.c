c07565e6d202c32ea618cb5a0002e5ffffe87392
Po Bidau <pobidau@protonmail.com>
2026-04-06 15:18:38 +0200

    chore: creation des fonctions d'init

--- src/main.c
+++ src/main.c
@@ -8,51 +8,22 @@
 
 // Cell grid[ROWS][COLUMNS];
 
+// Variables globales SDL
+static SDL_Window *window = NULL; // Pointeur vers la fenêtre SDL
+static SDL_Renderer *renderer = NULL; // Pointeur vers le renderer
+
+// Foncitons d'initalisation
+void init_sdl(void);
+void cleanup(void);
+
+// -------------------------------------------------------------------
+
 int main(int argc, char* argv[])
 {
-    // --------------------------------------------------------
-    // VARIABLES
-    // --------------------------------------------------------
-    SDL_Window *window = NULL; // Pointeur vers la fenêtre SDL
-    SDL_Renderer *renderer = NULL; // Pointeur vers le renderer
 
-    // --------------------------------------------------------
-    // INITIALISATION
-    // --------------------------------------------------------
-
-    // Initialisation de SDL (système vidéo ici)
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        fprintf(stderr, "Erreur SDL_Init: %s\n", SDL_GetError());
-        goto cleanup;
-    }
-
-    // Création de la fenêtre
-    window = SDL_CreateWindow(
-        "Liquide Simulation",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        SCREEN_WIDTH, SCREEN_HEIGHT,
-        SDL_WINDOW_SHOWN);
-
-    if(window == NULL)
-    {
-        fprintf(stderr, "Erreur SDL_CreateWindow: %s\n", SDL_GetError());
-        goto cleanup;
-    }
-
-    // Création du renderer (outils de dessin)
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(renderer == NULL)
-    {
-        fprintf(stderr, "Erreur SDL_CreateRenderer : %s\n", SDL_GetError());
-        goto cleanup;
-    }
-
-    // --------------------------------------------------------
-    // BOUCLE PRINCIPALE
-    // --------------------------------------------------------
-
+    init_sdl();
     init_grid();
+
     SDL_bool program_running = SDL_TRUE;
     SDL_Event event; // capte tous les event
     SDL_bool delete_mode = SDL_FALSE;
@@ -118,17 +89,53 @@ int main(int argc, char* argv[])
         SDL_Delay(16);
     }
 
+    cleanup();
 
+    return 0;
+}
 
-// --------------------------------------------------------
-// Label pour le nettoyage
-// --------------------------------------------------------
-cleanup:
+// -------------------------------------------------------------------
+
+void init_sdl(void)
+{
+    // Initialisation de SDL (système vidéo ici)
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        fprintf(stderr, "Erreur SDL_Init: %s\n", SDL_GetError());
+        exit(1); // rien à nettoyer, SLD n'est pas initilisé;
+    }
+
+    // Création de la fenêtre window
+    window = SDL_CreateWindow(
+        "Liquide Simulation",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
+
+    if(window == NULL)
+    {
+        fprintf(stderr, "Erreur SDL_CreateWindow: %s\n", SDL_GetError());
+        SDL_Quit(); // SDL est déjà initialisé, on le nettoie
+        exit(1);
+    }
+
+    // Création du renderer (outils de dessin)
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(renderer == NULL)
+    {
+        fprintf(stderr, "Erreur SDL_CreateRenderer : %s\n", SDL_GetError());
+        cleanup();
+
+    }
+}
+
+// -------------------------------------------------------------------
+
+void cleanup(void)
+{
     if (renderer) SDL_DestroyRenderer(renderer);
     if (window) SDL_DestroyWindow(window);
     SDL_Quit();
-
-    return 0;
 }
 
 
