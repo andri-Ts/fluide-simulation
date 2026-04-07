6c01b53cb625b1a1d9428e712805dfdb6de9a95b
andri-Ts <andri.ticy@ikmail.com>
2026-04-07 13:21:20 +0200

    Update render.c
    Suppression commentaire

--- src/render.c
+++ src/render.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-// #include <SDL2/SDL.h>
-// #include "../include/render.h"
-// #include "../include/fluide.h"
-
 #include <SDL2/SDL.h>
 #include "render.h"
 #include "fluide.h"
@@ -44,19 +40,7 @@ void render_frame(void)
     SDL_RenderPresent(renderer);
 }
 
-// void render_frame(SDL_Renderer *renderer)
-// {
-//     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-//     SDL_RenderClear(renderer); // efface TOUT
-
-//     draw_cell(renderer);
-//     draw_grid(renderer);
-
-//     // OPTIONS AFFICHAGES
-//     SDL_RenderPresent(renderer); // "affiche" tout ce qu'on a dessiné
-// }
-
-// // ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
 
 void draw_grid(SDL_Renderer *renderer)
 {
@@ -80,44 +64,3 @@ void draw_grid(SDL_Renderer *renderer)
         SDL_RenderDrawLine(renderer, 0, posY, COLUMNS * CELL_SIZE, posY);
     }
 }
-
-// // ----------------------------------------------------------------------------------------
-
-// void draw_cell(SDL_Renderer *renderer)
-// {
-//     for(int y = 0; y < ROWS; y++) // parcours les lignes (0 -> ROWS)
-//     {
-//         for (int x = 0; x < COLUMNS; x++) // parcours les colonnes (0 à COLUMNS)
-//         {
-//             if(grid[y][x].type == EMPTY_TYPE)
-//                 continue; // cellule vide, on ne dessine rien
-
-//             if(grid[y][x].type == SOLID_TYPE)
-//             {
-//                 SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE); // determiner la couleur (blanc)
-//                 SDL_Rect cell = {
-//                     x * CELL_SIZE,
-//                     y * CELL_SIZE,
-//                     CELL_SIZE,
-//                     CELL_SIZE
-//                 };
-//                 SDL_RenderFillRect(renderer, &cell);
-//             }
-//             else if(grid[y][x].type == WATER_TYPE)
-//             {
-//                 SDL_SetRenderDrawColor(renderer, 0, 180, 216, SDL_ALPHA_OPAQUE); // eau bleu
-//                 // L'eau doit oller au bas de la cellule
-//                 int height_water = CELL_SIZE * grid[y][x].fill_level; // si level = 1, taille de l'eau 12px, si level 0.75, taille de l'eau : 12 * 0.75
-//                 int posY_water = grid[y][x].y * CELL_SIZE + (CELL_SIZE - height_water); // y = top of cell + (cellsize - height of water)
-
-//                 SDL_Rect cell = {
-//                     grid[y][x].x * CELL_SIZE,
-//                     posY_water,
-//                     CELL_SIZE,
-//                     height_water
-//                 };
-//                 SDL_RenderFillRect(renderer, &cell);
-//             }
-//         }
-//     }
-// }
