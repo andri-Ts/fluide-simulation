36c7fc8302f2a5f6f10e0a7e167a70af1a6cfa88
Po Bidau <pobidau@protonmail.com>
2026-04-08 10:29:13 +0200

    chore: sauvegarde du bug du gravite

--- src/fluide.c
+++ src/fluide.c
@@ -1,4 +1,5 @@
 #include "fluide.h"
+#include <stdio.h>
 
 // définition réelle de la grille
 static Cell grid[ROWS][COLUMNS]; // visible uniquement dans ce fichier
@@ -31,6 +32,36 @@ void init_grid(void)
 
 // ----------------------------------------------------------------------------------------
 
+// void simulation_step(void)
+// {
+//     for(int y = ROWS - 2; y >= 0; y--)
+//     {
+//         for(int x = 0; x < COLUMNS; x++)
+//         {
+//             switch(grid[y][x].type)
+//             {
+//                 case EMPTY_TYPE:
+//                     break;
+
+//                 case WATER_TYPE: // je suis une cellule d'eau
+//                     if(grid[y+1][x].type == EMPTY_TYPE) // si la cellule de dessous est vide
+//                     {
+//                         grid[y+1][x].type = WATER_TYPE; // elle reçoit mon eau
+//                         grid[y+1][x].fill_level = grid[y][x].fill_level; // avec la meme quantité
+
+//                         grid[y][x].type = EMPTY_TYPE;  // moi je me vide
+//                         grid[y][x].fill_level = 0.0f;
+
+//                     }
+//                     break;
+
+//                 default:
+//                     break;
+//             }
+//         }
+//     }
+// }
+
 void simulation_step(void)
 {
     for(int y = ROWS - 2; y >= 0; y--) // -2 : car on regarde la cellule d'en dessous
@@ -39,20 +70,55 @@ void simulation_step(void)
         {
             if(grid[y][x].type == WATER_TYPE) // je suis une cellule d'eau
             {
-                switch(grid[y+1][x].type)
+                // on agis directement sur la cellule et non une copie (facilite la tache aulieu de faire grid[y][x] à chque fois)
+                Cell *current_c = &grid[y][x];
+                Cell *below_c = &grid[y+1][x];
+
+                switch(current_c->type)
                 {
-                    case EMPTY_TYPE: // si la cellule de dessous es vide, j'y met ma quantité d'eau et je me vide
-                        grid[y+1][x].type = WATER_TYPE;
-                        grid[y+1][x].fill_level = grid[y][x].fill_level;
-                        grid[y][x].type = EMPTY_TYPE;
-                        grid[y][x].fill_level = 0.0f;
+                    case EMPTY_TYPE: // si la cellule de dessous est vide, j'y met ma quantité d'eau et je me vide
+                        below_c->type = WATER_TYPE;
+                        below_c->fill_level = current_c->fill_level;
+                        current_c->type = EMPTY_TYPE;
+                        current_c->fill_level = 0.0f;
                         break;
 
+                        // grid[y+1][x].type = WATER_TYPE;
+                        // grid[y+1][x].fill_level = grid[y][x].fill_level;
+                        // grid[y][x].type = EMPTY_TYPE;
+                        // grid[y][x].fill_level = 0.0f;
+                        // break;
+
                     case WATER_TYPE:
-                        double availability = 1 - grid[y+1][x].fill_level; // calculer le reste qu'il peut recevoir
-                        grid[y+1][x].fill_level = grid[y+1][x].fill_level + availability; // // ajouter une partie de l'eau de dessous selon la disponibilté
-                        grid[y][x].fill_level = grid[y][x].fill_level - availability; // retiré cette partie d'eau à celui d'au dessous
+                    {
+                        // float below_capacity = 1.0 - below_c->fill_level; // Combien peut recevoir la cellule du dessous
+                        // float current_available = current_c->fill_level; // Combien peut donner la cellule du dessus
+
+                        // // La quantité réel transmis vers le bas est le minimum entre ce que le bas peut recevoir et ce que le haut peut donner
+                        // // Si below_capacity = 0; transfer = 0 => belew_c ne peut plus recevoir de flux
+                        // float transfert = (below_capacity < current_available) ? below_capacity : current_available;
+                        // if(transfert > 0.0f) //on ne transfère que si c'est positif()
+                        // {
+                        //     below_c->fill_level += transfert;
+                        //     current_c->fill_level -= transfert; // on retire ce qui a été pu transféré au dessous
+
+                        //     if(current_c->fill_level == 0.0f) // si la cellule courante est vide après transfert, on la marque comme vide
+                        //     {
+                        //         current_c->type = EMPTY_TYPE;
+                        //         current_c->fill_level = 0.0f;
+                        //     }
+                        // }
+
+                        // below_c->fill_level += current_c->fill_level;  // ajouter une partie de l'eau de dessous selon la disponibilté
+                        // current_c->fill_level -= below_capacity; // retiré cette partie d'eau à celui d'au dessous
+
+                        // double availability = 1 - grid[y+1][x].fill_level; // calculer le reste qu'il peut recevoir
+                        // grid[y+1][x].fill_level = grid[y+1][x].fill_level + availability; // // ajouter une partie de l'eau de dessous selon la disponibilté
+                        // grid[y][x].fill_level = grid[y][x].fill_level - availability; // retiré cette partie d'eau à celui d'au dessous
+                        // break;
+
                         break;
+                    }
 
                     default:
                         break;
@@ -160,13 +226,49 @@ void simulation_step(void)
 //     }
 // }
 
+// void simulation_step(void)
+// {
+//     for(int y = ROWS - 2; y >= 0; y--) // -2 : car on regarde la cellule d'en dessous
+//     {
+//         for(int x = 0; x < COLUMNS; x++)
+//         {
+//             if(grid[y][x].type == WATER_TYPE) // je suis une cellule d'eau
+//             {
+//                 // on agis directement sur la cellule et non une copie (facilite la tache aulieu de faire grid[y][x] à chque fois)
+//                 Cell *current_c = &grid[y][x];
+//                 Cell *below_c = &grid[y+1][x];
+
+//                 switch(grid[y][x].type)
+//                 {
+//                     case EMPTY_TYPE: // si la cellule de dessous es vide, j'y met ma quantité d'eau et je me vide
+//                         grid[y+1][x].type = WATER_TYPE;
+//                         grid[y+1][x].fill_level = grid[y][x].fill_level;
+//                         grid[y][x].type = EMPTY_TYPE;
+//                         grid[y][x].fill_level = 0.0f;
+//                         break;
+
+//                     case WATER_TYPE:
+//                         double availability = 1 - grid[y+1][x].fill_level; // calculer le reste qu'il peut recevoir
+//                         grid[y+1][x].fill_level = grid[y+1][x].fill_level + availability; // // ajouter une partie de l'eau de dessous selon la disponibilté
+//                         grid[y][x].fill_level = grid[y][x].fill_level - availability; // retiré cette partie d'eau à celui d'au dessous
+//                         break;
+
+//                     default:
+//                         break;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
 // ----------------------------------------------------------------------------------------
 
 void water_cell(int x, int y)
 {
     if(x < 0 || x >= COLUMNS || y < 0 || y >= ROWS) return;
     grid[y][x].type = WATER_TYPE;
-    grid[y][x].fill_level = 0.30;
+    grid[y][x].fill_level = 0.70;
 }
 
 void solid_cell(int x, int y)
